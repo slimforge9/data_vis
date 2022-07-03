f33a47b474d714a5a540f19177e638127f4cd831
elonymac <elonymac@gmail.com>
2022-07-03 13:19:12 +0200

    kostka i wizualizacja wyrzutów

---
+++ die_visual.py
@@ -0,0 +1,32 @@
+from plotly.graph_objs import Bar, Layout
+from plotly import offline
+
+
+from die import Die
+
+# Utworzenie kości typu D6
+die = Die()
+
+# Wykonanie pewnej liczby rzutów ii umeiszczenie wyników na liśćie.
+results = []
+for roll_num in range(1000):
+    result = die.roll()
+    results.append(result)
+
+# Analiza wyników
+frequencies = []
+for value in range(1, die.num_sides+1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+# Wizualizacja wyników
+x_values = list(range(1, die.num_sides+1))
+data = [Bar(x=x_values, y=frequencies)]
+
+x_axis_config = {'title': 'Wynik'}
+y_axis_config = {'title': 'Częstotliwość występowania wartości'}
+my_layout = Layout(title='Wynik rzucania pojedynczą kością D6 tysiąc razy', xaxis=x_axis_config, yaxis=y_axis_config)
+offline.plot({'data': data, 'layout': my_layout}, filename='d6.html')
+
+print(frequencies)
+
